acb7fe6e9a3a4e4a5a917a79b8ebf124bd379e5f
Sachin <sachincodes1@gmail.com>
2023-01-13 22:46:31 +0530

    Day 19 commit, BUSY

---
+++ Day 19/TALLER.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int x,y;
+        cin>>x>>y;
+        if(x>y) cout<<"A"<<endl;
+        else cout<<"B"<<endl;
+    }
+	return 0;
+}
