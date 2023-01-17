370033b4f394f6f3ac49f6d7f384852ad4a87531
Sachin <sachincodes1@gmail.com>
2023-01-17 22:53:02 +0530

    none

---
+++ Day 23/SUBSCRIBE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,x;
+	cin>>T;
+	while(T--){
+	    cin>>x;
+	    if(x<=30) cout<<"NO\n";
+	    else cout<<"YES\n";
+	}
+	    
+	
+	return 0;
+}
