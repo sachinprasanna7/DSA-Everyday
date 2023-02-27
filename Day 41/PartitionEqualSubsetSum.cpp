ff8d4f0a3598e491619e07a173311c4ad33fc47d
Sachin <sachincodes1@gmail.com>
2023-02-28 00:21:29 +0530

    equal subsets

--- Day 41/PartitionEqualSubsetSum.cpp
+++ Day 41/PartitionEqualSubsetSum.cpp
@@ -10,6 +10,9 @@ using namespace std;
 class Solution {
     private:
         
+
+        //BASIC RECURSION - TLE
+
         bool solve(vector <int> &nums, int i, int startSum, int endSum){
             
             if(startSum == endSum) return true;
@@ -23,12 +26,43 @@ class Solution {
 
         }
 
+        //RECURSION + MEMORISATION
+
+        int solveMem(vector <int> &nums, int i, int startSum, vector <vector <int>> &dp ){
+            
+            
+            if(i >= nums.size()) return 0;
+
+            if(startSum < 0) return 0;
+
+            if(startSum == 0) return 1;
+
+            if(dp[i][startSum] != -1) return dp[i][startSum];
+
+            int ans1 = solveMem(nums, i+1, startSum - nums[i], dp);
+            int ans2 = solveMem(nums, i+1, startSum, dp);
+
+
+            dp[i][startSum] =  ans1 || ans2;
+
+            return dp[i][startSum];
+
+        }
+
 public:
     bool canPartition(vector<int>& nums) {
         
         int sumArray = accumulate(nums.begin(), nums.end(), 0);
 
-        return solve(nums, 0, 0, sumArray);
+        if(sumArray % 2 == 1) return false;
+        sumArray /= 2;
+
+        vector <vector <int>> dp (nums.size(), vector<int> (sumArray + 1, -1));
+
+        int ans = solveMem(nums, 0, sumArray, dp);
+        if (ans) return true;
+
+        return false;
 
     }
 };
