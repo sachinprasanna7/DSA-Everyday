9ef1cab2ed796dc8aaabbc62fd55910c2a39650b
Sachin <sachincodes1@gmail.com>
2023-02-28 22:26:09 +0530

    to lower case

---
+++ Day 42/ToLowerCase.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        for(int i = 0 ; i < s.size() ; i++){
+            s[i]= tolower(s[i]);
+        }
+        return s;
+    }
+};
+
+int main(){
+  
+  return 0;
+}
