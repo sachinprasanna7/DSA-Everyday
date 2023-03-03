86d91733b2b3424f056f7f7631c21acd35a2ed10
Sachin <sachincodes1@gmail.com>
2023-03-03 07:45:47 +0530

    march 3 daily challenge

---
+++ Day 45/DailyChallenge3_3_23.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        size_t found = haystack.find(needle);
+
+        if (found != string::npos) return found;
+
+        return -1;
+    }
+};
+
+
+//USE OF INT
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int found = haystack.find(needle);
+
+        if (found != string::npos) return found;
+
+        return -1;
+    }
+};
+
+int main(){
+  
+  return 0;
+}
