3d386e3b2c417c1ef5f9b09e993844c2ac47bab1
Sachin <sachincodes1@gmail.com>
2023-03-03 23:07:14 +0530

    march 3 commit

---
+++ Day 45/BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+  struct TreeNode {
+      int val;
+     TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+    private:
+        void inorder(vector <int> &ans, TreeNode* root){
+
+            if(root == nullptr) return;
+
+            inorder(ans, root -> left);
+            ans.push_back(root -> val);
+            inorder(ans, root -> right);
+
+        }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+
+        vector <int> ans;
+        inorder(ans, root);
+        return ans;
+    }
+};
+
+int main(){
+  
+  return 0;
+}
