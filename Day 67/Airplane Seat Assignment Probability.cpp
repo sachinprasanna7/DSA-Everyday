9701e9b47e1e888631d1f19e668c613062cc66e7
Sachin <sachincodes1@gmail.com>
2023-03-26 23:22:49 +0530

    airplane probability

---
+++ Day 67/Airplane Seat Assignment Probability.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Solution{
+public:
+    double nthPersonGetsNthSeat(int n) {
+
+        if(n >= 2) return 0.50000;
+        return 1.00000;
+    }
+};
+
+int main(){
+  
+  return 0;
+}
